a708d417945cb9db6043e53d26fd21479c82af5b
Josh Glovak <glovak@innereactive.com>
2018-09-25 12:20:11 -0400

    Script rewrite and language addition
    - Rewrote script to be more dynamic for languages
    - Added spanish translations

--- interactive-eye/fields.php
+++ interactive-eye/fields.php
@@ -20,11 +20,13 @@ function interactive_eye_func( $atts, $content = "")
 		'language' => 'english',
 	], $atts);
 	
+	$language = $a["language"];
+	
 	//Blank Interactive Eye Output
 	$interactive_eye_output = '';
 	
 	$interactive_eye_output .= '<!-- Interactive Eye -->';
-		$interactive_eye_output .= '<div id="interactive-eye-wrapper" class="interactive-eye-wrapper ' . $a["language"] . '-language">';
+		$interactive_eye_output .= '<div id="interactive-eye-wrapper" class="interactive-eye-wrapper ' . $language . '-language">';
 			
 			$interactive_eye_output .= '<div id="contain23">';
 				$interactive_eye_output .= '<!-- Eye Structure -->';
@@ -45,20 +47,41 @@ function interactive_eye_func( $atts, $content = "")
 				$interactive_eye_output .= '<br>';
 				
 				$interactive_eye_output .= '<!-- Toggles -->';
-				$interactive_eye_output .= '<button class="eyelid"><span class="name">Eyelid</span></button>';
-				$interactive_eye_output .= '<button  class="cornea"><span class="name">Cornea</span></button>';
-				$interactive_eye_output .= '<button class="iris"><span class="name">Iris</span></button>';
-				$interactive_eye_output .= '<button class="pupil"><span class="name">Pupil</span></button>';
-				$interactive_eye_output .= '<button class="lens"><span class="name">Lens</span></button>';
-				$interactive_eye_output .= '<button class="fovea"><span class="name">Fovea</span></button>';
-				$interactive_eye_output .= '<button class="macula"><span class="name">Macula</span></button>';
-				$interactive_eye_output .= '<button class="retina"><span class="name">Retina</span></button>';
-				$interactive_eye_output .= '<button class="nerve"><span class="name">Optic Nerve</span></button>';
-				$interactive_eye_output .= '<button class="vitreous"><span class="name">Vitreous</span></button>';
+				
+				//Spanish toggles
+				if($language == 'spanish')
+				{
+					$interactive_eye_output .= '<button class="eyelid" data-language="spanish" data-language-content="Pliegues musculares superiores e inferiores de la piel que se mueven para cubrir y descubrir el globo ocular."><span class="name">Párpado</span></button>';
+					$interactive_eye_output .= '<button class="cornea" data-language="spanish" data-language-content="Capa frontal transparente del ojo que cubre el iris y la pupila. Proporciona la mayor parte de la potencia óptica del ojo."><span class="name">Córnea</span></button>';
+					$interactive_eye_output .= '<button class="iris" data-language="spanish" data-language-content="Estructura delgada y circular en el ojo, responsable de controlar el diámetro y el tamaño de la pupila y, por lo tanto, la cantidad de luz que llega a la retina."><span class="name">Iris</span></button>';
+					$interactive_eye_output .= '<button class="pupil" data-language="spanish" data-language-content="Abertura oscura y circular en el centro del iris que varía en tamaño a la cantidad regular de luz que llega a la retina."><span class="name">Pupila</span></button>';
+					$interactive_eye_output .= '<button class="lens" data-language="spanish" data-language-content="Tejido biconvexo transparente que ayuda a que los rayos de luz se centren en la retina."><span class="name">Lente</span></button>';
+					$interactive_eye_output .= '<button class="fovea" data-language="spanish" data-language-content="Pequeña depresión en la retina donde la agudeza visual es máxima."><span class="name">Fovea</span></button>';
+					$interactive_eye_output .= '<button class="macula" data-language="spanish" data-language-content="Área irregularmente ovalada, pigmentada de amarillo en la retina central que contiene varillas sensibles al color y la agudeza visual más nítida de la región."><span class="name">Màcula</span></button>';
+					$interactive_eye_output .= '<button class="retina" data-language="spanish" data-language-content="Las capas celulares, sensibles a la luz, reciben imágenes producidas por la lente y activan impulsos nerviosos que pasan a través del nervio óptico hacia el cerebro."><span class="name">Retina</span></button>';
+					$interactive_eye_output .= '<button class="nerve" data-language="spanish" data-language-content="Par de nervios craneales, que consta de fibras sensoriales que transmiten impulsos visuales al cerebro."><span class="name">Nervio óptico</span></button>';
+					$interactive_eye_output .= '<button class="vitreous" data-language="spanish" data-language-content="Masa transparente y gelatinosa que llena dos tercios del ojo entre la lente y la retina."><span class="name">Vítreo</span></button>';
+				}
+				
+				//English Toggles
+				if($language == 'english')
+				{
+					$interactive_eye_output .= '<button class="eyelid" data-language-content="Upper and lower muscular folds of skin that move to cover and uncover the eyeball."><span class="name">Eyelid</span></button>';
+					$interactive_eye_output .= '<button class="cornea" data-language="english" data-language-content="Transparent front layer of the eye that covers the iris and the pupil. Provides most of the eye\'s optical power."><span class="name">Cornea</span></button>';
+					$interactive_eye_output .= '<button class="iris" data-language="english" data-language-content="Flat, circular membrane forming the colored portion of the eye, with an adjustable circular opening, the pupil, in the center."><span class="name">Iris</span></button>';
+					$interactive_eye_output .= '<button class="pupil" data-language="english" data-language-content="Dark, circular opening in the center of the iris which varies in size to regular the amount of light reaching the retina."><span class="name">Pupil</span></button>';
+					$interactive_eye_output .= '<button class="lens" data-language="english" data-language-content="Transparent, biconvex tissue that helps bring rays of light to focus on the retina."><span class="name">Lens</span></button>';
+					$interactive_eye_output .= '<button class="fovea" data-language="english" data-language-content="Small depression in the retina where visual acuity is highest."><span class="name">Fovea</span></button>';
+					$interactive_eye_output .= '<button class="macula" data-language="english" data-language-content="Irregularly oval, yellow-pigmented area on the central retina that contains color-sensitive rods and the region sharpest vision acuity."><span class="name">Macula</span></button>';
+					$interactive_eye_output .= '<button class="retina" data-language="english" data-language-content="Cell layers, sensitive to light, receive images produced by the lens and triggers nerve impulses that pass via the optic nerve to the brain."><span class="name">Retina</span></button>';
+					$interactive_eye_output .= '<button class="nerve" data-language="english" data-language-content="Pair of cranial nerves, consisting of sensory fibers that transmit sight impulses to the brain."><span class="name">Optic Nerve</span></button>';
+					$interactive_eye_output .= '<button class="vitreous" data-language="english" data-language-content="Transparent, gelatinous mass which fills two-thirds of the eye between the lens and the retina."><span class="name">Vitreous</span></button>';
+				}
+				
 				
 				$interactive_eye_output .= '<!-- Description -->';
 				$interactive_eye_output .= '<p>';
-					$interactive_eye_output .= '<span class="description" id="description" style="display: block;">';
+					$interactive_eye_output .= '<span class="description ' . $language . '-description" id="description" style="display: block;">';
 					$interactive_eye_output .= '</span>';
 				$interactive_eye_output .= '</p>';
 			$interactive_eye_output .= '</div>';
--- interactive-eye/js/script.js
+++ interactive-eye/js/script.js
@@ -19,70 +19,120 @@ jQuery(document).ready(function($)
 	$(".eyelid").click(function()
 	{
 		showonly(".eyelidimage");
-		document.getElementById("description").innerHTML = "Upper and lower muscular folds of skin that move to cover and uncover the eyeball.";
-		$("#description").show();
+		$language = $(this).data('language');
+		$content = $(this).data('language-content');
+		$target = '#description.' + $language + '-description';
+		
+		$($target).html($content);
+		
+		$($target).show();
 	});
 	
 	$(".cornea").click(function()
 	{
 		showonly(".corneaimage");
-		document.getElementById("description").innerHTML = "Transparent front layer of the eye that covers the iris and the pupil. Provides most of the eye\'s optical power.";
-		$("#description").show();
+		$language = $(this).data('language');
+		$content = $(this).data('language-content');
+		$target = '#description.' + $language + '-description';
+		
+		$($target).html($content);
+		
+		$($target).show();
 	});
 	
 	$(".pupil").click(function()
 	{
 		showonly(".pupilimage");
-		document.getElementById("description").innerHTML = "Dark, circular opening in the center of the iris which varies in size to regular the amount of light reaching the retina.";
-		$("#description").show();
+		$language = $(this).data('language');
+		$content = $(this).data('language-content');
+		$target = '#description.' + $language + '-description';
+		
+		$($target).html($content);
+		
+		$($target).show();
 	});
 	
 	$(".lens").click(function()
 	{
 		showonly(".lensimage");
-		document.getElementById("description").innerHTML = "Transparent, biconvex tissue that helps bring rays of light to focus on the retina.";
-		$("#description").show();
+		$language = $(this).data('language');
+		$content = $(this).data('language-content');
+		$target = '#description.' + $language + '-description';
+		
+		$($target).html($content);
+		
+		$($target).show();
 	});
 	
 	$(".retina").click(function()
 	{
 		showonly(".retinaimage");
-		document.getElementById("description").innerHTML = "Cell layers, sensitive to light, receive images produced by the lens and triggers nerve impulses that pass via the optic nerve to the brain.";
-		$("#description").show();
+		$language = $(this).data('language');
+		$content = $(this).data('language-content');
+		$target = '#description.' + $language + '-description';
+		
+		$($target).html($content);
+		
+		$($target).show();
 	});
 	
 	$(".nerve").click(function()
 	{
 		showonly(".nerveimage");
-		document.getElementById("description").innerHTML = "Pair of cranial nerves, consisting of sensory fibers that transmit sight impulses to the brain.";
-		$("#description").show();
+		$language = $(this).data('language');
+		$content = $(this).data('language-content');
+		$target = '#description.' + $language + '-description';
+		
+		$($target).html($content);
+		
+		$($target).show();
 	});
 	
 	$(".macula").click(function()
 	{
 		showonly(".maculaimage");
-		document.getElementById("description").innerHTML = "Irregularly oval, yellow-pigmented area on the central retina that contains color-sensitive rods and the region sharpest vision acuity.";
-		$("#description").show();
+		$language = $(this).data('language');
+		$content = $(this).data('language-content');
+		$target = '#description.' + $language + '-description';
+		
+		$($target).html($content);
+		
+		$($target).show();
 	});
 	
 	$(".fovea").click(function()
 	{
 		showonly(".foveaimage");
-		document.getElementById("description").innerHTML = "Small depression in the retina where visual acuity is highest.";
-		$("#description").show();
+		$language = $(this).data('language');
+		$content = $(this).data('language-content');
+		$target = '#description.' + $language + '-description';
+		
+		$($target).html($content);
+		
+		$($target).show();
 	});
 	
 	$(".vitreous").click(function()
 	{
 		showonly(".vitreousimage");
-		document.getElementById("description").innerHTML = "Transparent, gelatinous mass which fills two-thirds of the eye between the lens and the retina.";
-		$("#description").show();
+		$language = $(this).data('language');
+		$content = $(this).data('language-content');
+		$target = '#description.' + $language + '-description';
+		
+		$($target).html($content);
+		
+		$($target).show();
 	});
 	
 	$(".iris").click(function()
 	{
 		showonly(".irisimage");
-		document.getElementById("description").innerHTML = "Flat, circular membrane forming the colored portion of the eye, with an adjustable circular opening, the pupil, in the center.";
-		$("#description").show();
+		$language = $(this).data('language');
+		$content = $(this).data('language-content');
+		$target = '#description.' + $language + '-description';
+		
+		$($target).html($content);
+		
+		$($target).show();
 	});
 });
